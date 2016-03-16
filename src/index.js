5a36fd4c5c724c5db16075a440503d6963b966f4
zacanger <zac@zacanger.com>
2016-03-15 21:35:15 -0600

    :fork_and_knife: just some extra handling in the if statements

--- src/index.js
+++ src/index.js
@@ -1,38 +1,49 @@
 #!/usr/bin/env node
 
-var gs        = require('github-scraper')
-  , user      = process.argv[2]
-  , query     = process.argv[3]
-  , repos     = '?tab=repositories'
-  , activity  = '?tab=activity'
-  , followers = '/followers'
-  , following = '/following'
-  , url
+'use strict'
 
+const
+  gs        = require('github-scraper')
+, user      = process.argv[2]
+, query     = process.argv[3]
+, repos     = '?tab=repositories'
+, activity  = '?tab=activity'
+, followers = '/followers'
+, following = '/following'
+
+let url
+
+// no results
 if(!user){
-  console.log('please provide a username, and optionally a query\nerror:')
+  console.log('please provide a username, and optionally a query.')
 }
 
-if(query && query == 'repos'){
-  url = user + repos
-}
-else if(query && query == 'activity'){
-  url = user + activity
-}
-else if(query && query == 'followers'){
-  url = user + followers
-}
-else if(query && query == 'following'){
-  url = user + following
-}
-else {
-  url = user
+// right now, only one query is processed.
+// i'll work on this.
+if(process.argv[4]){
+  console.log('one query at a time, please!')
 }
 
-gs(url, function(err, data){
-  if(err){
-    console.error(err)
+else if(user || query){
+  if(query == 'repos'){
+    url = user + repos
+  } else if(query == 'activity'){
+    url = user + activity
+  } else if(query == 'followers'){
+    url = user + followers
+  } else if(query == 'following'){
+    url = user + following
+  } else if(user){
+    url = user
   }
-  console.log(JSON.stringify(data, null, 2))
-})
+
+  // gs returns objects; we want json
+  gs(url, function(err, data){
+    if(err){
+      console.error(err)
+    }
+    console.log(JSON.stringify(data, null, 2))
+  })
+
+}
 
